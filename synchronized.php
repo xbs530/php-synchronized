9d4e71c1820afbc45e41fbfd93eccb8d9272e444
xiongbaoshan <987188548@qq.com>
2016-10-31 15:04:01 +0800

    增加锁超时，解决死锁问题

--- synchronized.php
+++ synchronized.php
@@ -34,14 +34,48 @@ if(!function_exists('synchronized')){
 
 
         private function lock_start($lock_name,$block=true){
-            $file_path="/tmp/synchronized_{$lock_name}.tmp";
-            touch($file_path);
-            $this->lock_handle=fopen($file_path,'w');
-            if(!$this->lock_handle){
-                return false;
-            }
-            $lock_mode=$block?LOCK_EX:(LOCK_EX|LOCK_NB);
-            return flock($this->lock_handle,$lock_mode);
+
+            do {
+                $file_path = "/tmp/synchronized_{$lock_name}.tmp";
+
+                //死锁处理
+                if (is_file($file_path)) {
+                    $lock_time = file_get_contents($file_path);
+                    $lock_timeout = $lock_time + 60 * 10;
+                    if ($lock_time && $lock_timeout < time()) {
+                        unlink($file_path);
+                    }
+                }
+                touch($file_path);
+                $this->lock_handle = fopen($file_path, 'r+');
+                if (!$this->lock_handle) {
+                    return false;
+                }
+                $lock = flock($this->lock_handle, LOCK_EX | LOCK_NB);
+
+                //获得锁
+                if($lock){
+                    fwrite($this->lock_handle,time());
+                    break;
+
+                    //没得锁
+                }else{
+
+                    //阻塞模式
+                    if($block){
+                        $this->lock_end();
+                        sleep(10);
+
+                        //非阻塞模式
+                    }else{
+                        break;
+                    }
+
+                }
+
+            }while($block);
+
+            return $lock;
         }
 
         private function lock_end(){
